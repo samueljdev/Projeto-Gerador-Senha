824fbd11a312cc55b143ff4be2dc545fe66049f8
samueljdev <samuel.jdev11@gmail.com>
2025-07-24 15:41:16 -0400

    Finalizando a geração de senha

--- js/script.js
+++ js/script.js
@@ -20,7 +20,30 @@ const getSymbol = () => {
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = "";
+
+    const passwordLength = 10;
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol,
+    ];
+
+    for (i = 0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+            password += randomValue;
+        });
+    }
+    password = password.slice(0, passwordLength);
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+};
+
 // Eventos
 generatePasswordButton.addEventListener("click", (e) => {
-    console.log("teste");
+    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
 });
